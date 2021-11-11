671e78d1bae811d782794b9f6aa12ed702b0e5c3
DelfinMong <delfdevs@gmail.com>
2021-11-11 14:52:53 -0500

    reset

--- KeeperOfScore/app.js
+++ KeeperOfScore/app.js
@@ -4,6 +4,8 @@ const p2Button = document.querySelector('#p2Button')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
 
+const resetButton = document.querySelector('#reset')
+
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
@@ -27,6 +29,12 @@ p2Button.addEventListener('click',function (){
         }
         p2Display.textContent = p2Score;
     }
+})
 
-
+resetButton.addEventListener('click',function(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
 })
