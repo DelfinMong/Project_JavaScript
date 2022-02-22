52de159f498006e953e7bf284d77b19bcd0401d4
DelfinMong <delfdevs@gmail.com>
2022-02-21 23:50:43 -0500

    fetchquote

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,13 +1,22 @@
 let apiQuotes = []
 
-// Get Quotes From API
 
+// Show New Quote
+function newQuote(){
+  //Pick random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
+  console.log(quote);
+}
+
+
+// Get Quotes From API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes'
   try {
       const response = await fetch(apiUrl);
       apiQuotes = await response.json();
-      console.log(apiQuotes[12])
+      newQuote();
+      // console.log(apiQuotes[10])
   } catch(error) {
     //Catch Error Here
   }
