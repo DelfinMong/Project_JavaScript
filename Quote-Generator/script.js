1c1b8f961c497894fc77cf9efcfc66ce17ea7779
DelfinMong <delfdevs@gmail.com>
2022-02-25 23:28:26 -0500

    longquotecss

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -18,7 +18,20 @@ let apiQuotes = []
 function newQuote(){
   //Pick random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
-  authorText.textContent = quote.author;
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if(!quote.author){
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  
+  // Check Quote length to determine styling
+  if(quote.text.length > 50){
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
   quoteText.textContent = quote.text;
   console.log(quote)
 }
